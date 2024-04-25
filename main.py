04be13e275bdb01f722b6c18960996e62ed1c165
GabrielVaya <gabyvaya@gmail.com>
2024-04-25 10:18:13 +0200

    inicial

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+
+from utils.loadtoMongo import MongoLoader
+
+def main():
+    
+    #######################Load data to MongoDB#####################
+    mongoLoader = MongoLoader()
+    ################################################################
+
+    #######################
+    return None 
+
+if __name__ == '__main__':
+    main()
---
+++ utils/loadtoMongo.py
@@ -0,0 +1,16 @@
+import os
+
+
+class MongoLoader():
+
+    def __init__(self):
+        
+        return None
+
+    def load_idealista(self):
+
+        return None
+    
+    def load_lookup(self):
+
+        return None
